f0132e44a1e959c6206532fd06a4f7a9ba26531e
MasakoIdei <tarte.groseilles@gmail.com>
2021-09-22 03:06:45 +0900

    練習問題０１

---
+++ src/Exercises01.java
@@ -0,0 +1,35 @@
+//Main～Main03の練習問題
+/*
+問題１
+画面に「縦幅３横幅５の長方形の面積は、15です。」という計算結果を表示させてください。
+ただし、変数名はそれぞれ
+縦幅　a, 横幅　b, 計算結果　c　をしようすること。
+確認出来たらコメントアウト
+
+public class Exercises01 {
+    public static void main(String[] args) {
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println("縦幅３横幅５の長方形の面積は、"+ c + "です。");
+    }
+}
+*/
+
+/*
+問２
+以下の５つの値を格納するときに適した値を考えて格納してみよう。
+①true　②あ　③3.14　④314159265853979L　⑤明日の降水確率は、30%だったよ
+ */
+
+public class Exercises01 {
+    public static void main(String[] args) {
+        boolean  choice = true;
+        char character = 'あ';
+        double PI = 3.14;
+        long number = 314159265853979L;
+        String word = "明日の降水確率は、30%だったよ";
+    }
+}
+
+
