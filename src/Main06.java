8d063dbfb7bbb363bbfbc08743506cd23a9c3d26
MasakoIdei <tarte.groseilles@gmail.com>
2021-09-29 00:03:25 +0900

    型の変換について

---
+++ src/Main06.java
@@ -0,0 +1,29 @@
+/*【型の変更】
+　Rubyだと、文字列と数字をそのまま結合出来なかったのに
+　Javaはなんで　＋　を利用しただけで結合出来るのか？
+　→評価の過程で、自動的に型を変換しているから
+ 　※ただし、bit数を超えてしまう場合（32bitの箱に64bitの箱を入れるようなこと）は基本的には出来ない。
+ 　　64bitの箱に入っていたデータが32bitの箱に入りきらない可能性がある為、エラーが発生する。
+ 　　因みに、元のデータの一部が抜けてしまってもいいから、強制的に変換したい場合は
+ 　　int age = (int)3.2; 　　←小数の方がデータ量が大きいのに無理やりintに押し込め
+ 　　で強制代入は出来なくもないらしい。ただし、制限あり。
+ 　　（int）の部分はキャスト演算子
+ 　　箱に収まりきらない部分は、捨てられてしまうため
+ 　　不具合が出たりすることあるらしく、おすすめはされていないらしい。
+ */
+
+public class Main06 {
+    public static void main(String[] args) {
+      // floatをdoubleに代入する時に、型も変換されている
+        float f = 3;
+        double d = f;
+        System.out.println(f);
+        System.out.println(d);
+
+        //エラーが発生するver
+//        double d = 3;    //64bit
+//        float f = d;     //32bit
+//        System.out.println(d);
+//        System.out.println(f);
+    }
+}
